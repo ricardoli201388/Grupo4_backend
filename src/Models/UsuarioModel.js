9bf8fc15231592e81e29e4d2908fc5c33a866199
Ricardo Li <ricardoli201388@gmail.com>
2023-05-24 16:05:20 -0300

    Criação de Models

---
+++ src/Models/UsuarioModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UsuarioSchema = new Schema; ({
+    email : String,
+    senha : String,
+    nome : String,
+    cargo : String
+})
+
+const UsuarioModel = mongoose.model('usuarios', UsuarioSchema);
+
+module.exports = UsuarioModel; 
